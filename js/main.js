cd45f85c9480821bf3c4674a43fa21c033e8e1cb
Ishan  Garg <ishan@Ishans-MacBook-Air.local>
2021-06-16 23:41:42 +0530

    change bachground url

--- js/main.js
+++ js/main.js
@@ -54,7 +54,7 @@ const speak = () => {
     }
     if(textInput.value !== ''){
         //Add background animation
-        body.style.background = '#141414 url(../img/wave.gif)';
+        body.style.background = '#141414 url(img/wave.gif)';
         body.style.backgroundRepeat = 'repeat-x';
         body.style.backgroundSize = '100% 100%';
 
